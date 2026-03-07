2b360493142dbdd3b454cc3f2d1c26a4304b4e8e
danwritecode <dnelson4993@outlook.com>
2026-03-07 13:35:45 -0500

    (fix) send + sync added to ChatHandler

--- src/services/chat_handler.rs
+++ src/services/chat_handler.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 use crate::types::LooperToolDefinition;
 
 #[async_trait]
-pub trait ChatHandler {
+pub trait ChatHandler: Send + Sync {
     async fn send_message(&mut self, message: &str) -> Result<()>;
     fn set_tools(&mut self, tools: Vec<LooperToolDefinition>);
 }
